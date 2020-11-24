6d8192db1d966a88d5729e30dc3ae82dab20cbdb
huwenguang <huwenguang@ebankunion.com>
2020-11-24 09:14:05 +0800

    delete comment

--- papp/pyclient.py
+++ papp/pyclient.py
@@ -8,48 +8,33 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 app = Flask(__name__)
 
 
-# This information is obtained upon registration of a new GitHub
 client_id = "foo"
 client_secret = "bar"
 authorization_base_url = 'http://localhost:8080/sso-server/oauth/authorize'
-# token_url = 'http://localhost:8080/sso-server/oauth/access_token'
 token_url = 'http://localhost:8080/sso-server/oauth/token'
 redirect_uri = 'http://localhost:8083/login'
 state = ''
 
 @app.route("/")
 def demo():
-    """Step 1: User Authorization.
-    Redirect the user/resource owner to the OAuth provider (i.e. Github)
-    using an URL with a few key OAuth parameters.
-    """
+
     github = OAuth2Session(client_id, redirect_uri=redirect_uri)
     authorization_url, state = github.authorization_url(authorization_base_url)
-    print 'Please go here and authorize,', authorization_url
-    sys.stdout.flush()
 
-    # State is used to prevent CSRF, keep this for later.
     session['oauth_state'] = state
     return redirect(authorization_url)
 
     
-# Step 2: User authorization, this happens on the provider.
 
 @app.route("/login", methods=["GET"])
 def login():
-    """ Step 3: Retrieving an access token.
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
-    """
 
     github = OAuth2Session(client_id, state=session['oauth_state'], redirect_uri=redirect_uri)
     token = github.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
 
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
     session['oauth_token'] = token
+    print '=================token:', token
+    sys.stdout.flush()
 
     return redirect(url_for('.profile'))
 
@@ -63,7 +48,6 @@ def profile():
 
 
 if __name__ == "__main__":
-    # This allows us to use a plain HTTP callback
     os.environ['DEBUG'] = "1"
 
     app.secret_key = os.urandom(24)
